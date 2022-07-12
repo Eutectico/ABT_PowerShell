0383fc3682420ac3f8d593359e08c4e598292a8a
Eutectico <fedejose@gmail.com>
2022-07-12 20:39:57 +0200

    Initiall commit

---
+++ Etc/Uninstall_SilverLight_Remote.ps1
@@ -0,0 +1,23 @@
+####################################################
+##  Remote Microsoft SilverLight Uninstall        ##
+##                                                ##
+##  NOTES:                                        ##
+##  Author: Federico Jose                         ##
+##  Create: 12 Jul 2022                           ##
+##                                                ##
+##  Modified:                                     ##
+##  Version 1.0 - Initial Script Creation         ##
+####################################################
+
+
+$Creds = Get-Credential
+$Client = Read-Host -Prompt 'Computer Name'
+$s = New-PSSession -ComputerName $Client -Credential $Creds
+
+#Invoke-Command -Session $s {wmic product where caption='Microsoft SilverLight' call uninstall}
+
+Invoke-Command -Session $s -ScriptBlock {$app = Get-WmiObject Win32_Product | Where-Object {$_.name -eq "Microsoft Silverlight"}
+                                        Write-Output $app
+                                         $app.Uninstall()}
+
+Remove-PSSession -Session $s
